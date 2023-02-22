eebca7285ac96f802a31e9cd0533d4017fc7e3f0
Aamna Suhail <aamnasuhail13213@gmail.com>
2023-02-22 23:46:27 +0530

    Replace useState with useReducer on the show page

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,15 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { apiGet } from "../misc/config";
 
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case "FETCH_SUCCESS": {
+      return { show: action.show, isLoading: false, error: null };
+    }
+    case "FETCH_FAILED": {
+      return { ...prevState, isLoading: false, error: action.error };
+    }
+    default:
+      return prevState;
+  }
+};
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+};
+
 const Show = () => {
   const { id } = useParams();
-  const [show, setShow] = useState(null);
+
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then((results) => {
-      setShow(results);
-    });
+    let isMounted = true;
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then((results) => {
+        if (isMounted) {
+          dispatch({ type: "FETCH_SUCCESS", show: results });
+        }
+      })
+      .catch((err) => {
+        if (isMounted) {
+          dispatch({ type: "FETCH_FAILED", error: err.message });
+        }
+      });
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
+
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
+  if (error) {
+    return <div>Error Occured : {error}</div>;
+  }
 
   return <div>Show</div>;
 };
